d2f2363d93c9018d655f87cca12061e1a9a98695
ArturS5 <151950794+ArturS5@users.noreply.github.com>
2024-05-10 20:05:50 +0300

    Update config_f.py

--- config_f.py
+++ config_f.py
@@ -1,7 +1,7 @@
-TOKEN = "7083933588:AAG1u27GGEeIXWmN0CxW9EaEJX94FMQYPUM"  # token телеграм-бота
+TOKEN = ""  # token телеграм-бота
 
-FOLDER_ID='b1gs35092smnokbou6me'
-IAM_TOKEN='t1.9euelZqNmZTNi86KmsuWm8rGjIuXi-3rnpWaipKLjJ6cnM-TjI7PkYyJjsjl8_cOPwdO-e8NWj15_N3z905tBE757w1aPXn8zef1656VmouOmZfGy8zHzcrJlcvInc3G7_zF656VmouOmZfGy8zHzcrJlcvInc3GveuelZqQkIvKjM7Mko-OkJnLl46Vk7XehpzRnJCSj4qLmtGLmdKckJKPioua0pKai56bnoue0oye.Hv11fEs3CjsGvJRy8zLdaI2Qi_T-Mn2HtixajgujfcNhpcYKYr3DHvz-2wqiVUbvg6DlbLrD_nSv1pvG50POBQ'
+FOLDER_ID=''
+IAM_TOKEN=''
 
 MAX_USERS = 3  # максимальное кол-во пользователей
 MAX_GPT_TOKENS = 10  # максимальное кол-во токенов в ответе GPT
